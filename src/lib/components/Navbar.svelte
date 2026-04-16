0ba4a34bb43ff42b11e0e022feaa70d6d5693de4
rasasaufar <rasasaufar4@gmail.com>
2026-04-16 11:16:59 +0700

    update button label to 'PORTOFOLIO NAUFAL'

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -92,7 +92,7 @@
 			? 'text-black'
 			: 'text-white'}"
 	>
-		NAUFAL GANTENG
+		PORTOFOLIO NAUFAL
 	</button>
 
 	<div class="font-headline hidden gap-12 text-xs font-bold tracking-tighter uppercase md:flex">
